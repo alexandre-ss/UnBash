b1c76dac6ee826393a0bf0da68eba29439815ef5
alexandre-ss <alexandresantana401@gmail.com>
2022-02-05 23:52:37 -0300

    name changing

--- helpers/bash.c
+++ helpers/bash.c
@@ -13,3 +13,9 @@ void ler_command_line(char *buffer, size_t bufsize){
     while(getline(&buffer, &bufsize, stdin) == 0);
     printf("%s", buffer);
 }
+
+void name_and_current_directory(char *name, char *directory){
+    printf("\033[0;32m");       
+    printf("UnB - %s - %s >> ", name, directory);
+    printf("\033[0m");
+}
--- main.c
+++ main.c
@@ -2,26 +2,24 @@
 #include "helpers/bash.c"
 
 int main(int argc, char **argv){
-    char *buffer;
+    char *command_buffer;
     size_t bufsize = 32;
     char cwd[PATH_MAX];
     char hostname[HOST_NAME_MAX+1];
 
     iniciar();
     while(1) {
-        printf("\033[0;32m");       
-        printf("UnB - %s - %s >> ",  getlogin(), getcwd(cwd, sizeof(cwd)));
-        printf("\033[0m");
-
-        buffer = (char *) malloc (bufsize * sizeof(char));
-        if (buffer == NULL) {
-            printf("Unable to allocate buffer");
+        name_and_current_directory(getlogin(), getcwd(cwd, sizeof(cwd)));
+        
+        command_buffer = (char *) malloc (bufsize * sizeof(char));
+        if (command_buffer == NULL) {
+            printf("Unable to allocate command");
             exit(-1);
         }
 
-        ler_command_line(buffer, bufsize);
+        ler_command_line(command_buffer, bufsize);
 
-        free(buffer);
+        free(command_buffer);
     }
 
     return 0;
